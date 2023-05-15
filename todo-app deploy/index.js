5d495cd511a72080f9de8c9c4591ef8941f4b6b4
RiyaSingla407 <singlariya407@gmail.com>
2023-05-15 20:10:40 +0530

    todo part 2 done

--- todo-app deploy/index.js
+++ todo-app deploy/index.js
@@ -1,32 +1,42 @@
 document.querySelector("form").addEventListener("submit", Todoapp);
-//document.getElementById("submit").addEventListener("click", addvalue);
-
+var todoarr= JSON.parse(localStorage.getItem("todo"))||[];
+displayTodos(todoarr);
 function Todoapp(){
-    event.preventDefault();
-    var row1 = document.createElement("tr");
-    var td1 = document.createElement("td");
-    td1.innerText= document.getElementById("task").value ;
-    var td2 = document.createElement("td");
-    td2.innerText= document.getElementById("priority").value ;
-    if(td2.innerText==="Low"){
-        td2.style.backgroundColor= "green";
-    }
-    else {
-        td2.style.backgroundColor= "red";
+    event.preventDefault();  
+    var todoobj = {
+       task:   document.getElementById("task").value,
+       priority :document.getElementById("priority").value,
     };
-
-    var td3 = document.createElement("td");
-    td3.innerText= "Delete"; 
-  td3.style.color= "red";
-
-    td3.addEventListener("click", removerow);
-    function removerow(){
-        event.target.parentNode.remove();
-          };
-    row1.append(td1,td2,td3); 
-   
-    document.querySelector("tbody").append(row1);
+    todoarr.push(todoobj);
+    localStorage.setItem("todo", JSON.stringify(todoarr));
+    displayTodos(todoarr);
 };
+function displayTodos(arr){
+    document.querySelector("tbody").innerText="";
+    arr.map(function(element){
+        var row1 = document.createElement("tr");
+        var td1 = document.createElement("td");
+        td1.innerText= element.task;
+        var td2 = document.createElement("td");
+        td2.innerText= element.priority ;
+        if( element.priority ==="Low"){
+            td2.style.backgroundColor= "green";
+        }
+        else {
+            td2.style.backgroundColor= "red";
+        };
+        var td3 = document.createElement("td");
+        td3.innerText= "Delete"; 
+       td3.style.color= "red";
+    
+        td3.addEventListener("click", removerow);
+        function removerow(){
+            event.target.parentNode.remove();
+              };
+        row1.append(td1,td2,td3);  
+        document.querySelector("tbody").append(row1);
+    })
+}
 
 
 
