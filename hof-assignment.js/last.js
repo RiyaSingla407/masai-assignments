012ab8c82f70703248c58291ea9f69b00b8e6da0
RiyaSingla407 <singlariya407@gmail.com>
2023-05-19 19:34:41 +0530

    sorting done

--- hof-assignment.js/last.js
+++ hof-assignment.js/last.js
@@ -1,6 +1,6 @@
 var arr =["assignment", "problem", "media", "upload"];
 var cry = arr.filter(function(element){
-    if(element[0]==="a"|| element[element.length-1==="a"])
+    if(element[0]==="a"|| element[element.length-1]==="a")
     {
         return (element);
     }
