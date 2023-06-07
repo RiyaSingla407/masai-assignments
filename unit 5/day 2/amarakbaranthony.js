c880ec831cf0bff6973f0f1f60a2c605953a38f8
RiyaSingla407 <singlariya407@gmail.com>
2023-06-07 11:43:34 +0530

    borrowing done

---
+++ unit 5/day 2/amarakbaranthony.js
@@ -0,0 +1,31 @@
+var amar= {
+    place:"Goa",
+    skill:"singer",
+
+}
+
+var akbar= {
+    skill:"Magician",
+    place:"Mumbai"
+}
+
+var anthony= {
+    place:"Kashmir",
+    skill:"chef",
+    skills:function(s){
+        this.skill=s
+    },
+   
+}
+anthony.skills.call(akbar, "chef")
+console.log(akbar)
+
+/*function objects(a,b){
+    this.skill=a,
+    this.place=b
+}
+
+var amar = new objects("singer", "Goa");
+var akbar = new objects("chef", "Mumbai");
+var anthony = new objects("Magician", "Kashmir");
+*/
